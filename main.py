2607ad8a12ec85e901f1f01ef3b36d2c0b49966d
Mathis Boulais <https://github.com/Asukyy>
2023-04-09 17:58:00 +0200

    end

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-# Créer un lien vers mon navigateur
 browser = webdriver.Chrome()
 
-# Charger le site web
 browser.get("https://10fastfingers.com/login")
 
-# Cliquer sur le bouton "Accepter tous les cookies"
 browser.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click()
 time.sleep(2)
 email_field = browser.find_element(By.ID, "UserEmail")
@@ -21,10 +18,8 @@ time.sleep(2)
 browser.find_element(By.ID, "login-form-submit").click()
 
 
-# Trouver la liste de mots et les stocker dans une variable
 words_list = browser.find_element(By.ID, "row1").find_elements(By.TAG_NAME, "span")
 
-# Écrire chaque mot dans le champ de saisie
 input_field = browser.find_element(By.ID, "inputfield")
 for word in words_list:
     input_field.send_keys(word.text + " ")
