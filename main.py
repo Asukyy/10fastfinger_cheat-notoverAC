c499181388cc29affb0ee2b9cf1616e851891baa
Mathis Boulais <https://github.com/Asukyy>
2023-04-09 18:06:04 +0200

    end

--- main.py
+++ main.py
@@ -7,23 +7,25 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome()
 
 browser.get("https://10fastfingers.com/login")
-
+time.sleep(2)
 browser.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click()
 time.sleep(2)
 email_field = browser.find_element(By.ID, "UserEmail")
-email_field.send_keys("tonMDP")
+email_field.send_keys("YOUR_EMAIL")
 password_field = browser.find_element(By.ID, "UserPassword")
-password_field.send_keys("tonemail")
+password_field.send_keys("YOUR_PASSWORD")
 time.sleep(2)
 browser.find_element(By.ID, "login-form-submit").click()
-
+time.sleep(2)
+browser.find_element(By.ID, "typing-test").click()
+time.sleep(3)
 
 words_list = browser.find_element(By.ID, "row1").find_elements(By.TAG_NAME, "span")
 
 input_field = browser.find_element(By.ID, "inputfield")
 for word in words_list:
     input_field.send_keys(word.text + " ")
-    time.sleep(0.3)
+    time.sleep(0.2)
 
 
 time.sleep(20000)
